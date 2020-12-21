6ec08ed22f8e850a34d0f74394d4a0ad74dc7bdc
AlexisLoya <alexloy117@gmail.com>
2020-12-20 22:32:49 -0600

    adding herencia in pytho

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
 var car = new Car("AW456", new Account("Andres Herrera", "QWE234"))
 car.passenger = 4;
 car.printDataCar();
+
+
+var uberX = new UberX("ADSR", new Account("Alexis Loya", "LXR23ll"), "Chevrolet", "Spark")
+uberX.passenger = 10
+uberX.printDataCar()
--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,8 @@
 class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passegenger;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passegenger;
 
     public Car(String license, Account driver){
         this.license = license;
@@ -12,4 +12,38 @@ class Car {
     void printDataCar() {
         System.out.println("License: " + license + " Driver: " + driver);
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    public Integer getPassegenger() {
+        return passegenger;
+    }
+
+    public void setPassegenger(Integer passegenger) {
+        this.passegenger = passegenger;
+    }
+
+    
 }
